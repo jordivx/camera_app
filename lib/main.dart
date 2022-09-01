51ba3201a7c0a2e61299d58c57de1129bdf57b73
Jordi Vila Xicola <jordivilaxicola@MacBook-Air-de-Jordi.local>
2022-09-01 11:55:14 +0200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:camera/camera.dart';
+import 'package:camera_app/camera_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure plugin services are initialized
+  final cameras = await availableCameras(); // Get list of available cameras
+
+  runApp(MyApp(cameras: cameras));
+}
+
+class MyApp extends StatelessWidget {
+
+  final List<CameraDescription> cameras;
+
+  const MyApp({Key? key, required this.cameras}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Camera App',
+      home: CameraScreen(cameras: cameras),
+    );
+  }
+}
